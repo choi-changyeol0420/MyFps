dd8ab7a6421f1bbc1fdd7d5d736adffc4aa17fd0
c55702980 <c55702980@naver.com>
2024-10-23 18:00:11 +0900

    1. 항아리 부수기 구현 2. 키 배치 및 항아리 배치 완료

---
+++ Assets/MyFps/Scripts/Breakable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Myfps
+{
+    public class Breakable : MonoBehaviour, IDamageable
+    {
+        public GameObject Barrel;
+        private float Health;
+        private float MaxHealth = 5;
+        private bool isDie = false;
+        public bool isKey = false;
+        public GameObject Key;
+
+        private void Start()
+        {
+            Health = MaxHealth;
+        }
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0 && !isDie)
+            {
+                Die();
+                if (!isKey) return;
+                if(isKey)
+                {
+                    Key.SetActive(true);
+                }
+            }
+        }
+        void Die()
+        {
+            isDie = true;
+            Destroy(gameObject);
+            GameObject Barrel_B = Instantiate(Barrel, transform.position, Quaternion.identity);
+        }
+    }
+}
